a566e8a67f13e681fc8b266a8e4cb42b55c5c4a0
bsp3725k <bhavanisivaprasad39@gmail.com>
2025-01-23 19:02:07 +0530

    File updated

--- src/components/App.js
+++ src/components/App.js
@@ -32,10 +32,19 @@ const App = () => {
     { name: 'Mount Abu', country: 'India' },
     { name: 'Tirupati', country: 'India' },
     ]
-    
+   // Filter cities that are in India
+  const indianCities = cityList.filter((city) => city.country === "India");
+
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Holiday List</h1>
+      <ol >
+        {indianCities.map((city, index) => (
+          // Adding a unique key in the format location1, location2, etc.
+          <li key={`location${index + 1}`} >{city.name}</li>
+        ))}
+      </ol>      
     </div>
   )
 }
